f480d349338c265347e194811662d0a33d6373a7
Levon Kirakosyan <noosx.e@gmail.com>
2023-05-26 20:29:33 -0700

    Add basic track simulation. (#2)

--- src/components/locomotive.rs
+++ src/components/locomotive.rs
@@ -2,7 +2,28 @@ use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct Locomotive {
-    target: Entity,
-    distance: f32,
-    speed: f32,
+    pub location: Entity,
+    pub target: u32,
+    pub distance: f32,
+    pub speed: f32,
+}
+
+impl Locomotive {
+    pub fn new(speed: f32, location: Entity, target: u32) -> Locomotive {
+        Locomotive {
+            location,
+            target,
+            distance: 0.0,
+            speed,
+        }
+    }
+
+    pub fn log_location(&self) {
+        println!(
+            "track: {}, target: {}, distance: {}",
+            self.location.index(),
+            self.target,
+            self.distance
+        )
+    }
 }
