424a57f2e3190b0b133ba5116089f5238ab3fd63
Levon Kirakosyan <noosx.e@gmail.com>
2023-05-26 20:24:15 -0700

    Add basic track simulation.

--- src/components/track.rs
+++ src/components/track.rs
@@ -1,19 +1,87 @@
 use bevy::prelude::*;
 
-struct Joint {
-    id: u32,
+pub struct Joint {
+    pub id: u32,
     connected: bool,
-    connected_entity: Option<Entity>,
+    pub connected_entity: Option<Entity>,
 }
 
-struct Connection {
-    distance: f32,
-    l1: u32,
-    l2: u32,
+impl Joint {
+    fn empty(id: u32) -> Joint {
+        Joint {
+            id,
+            connected: false,
+            connected_entity: None,
+        }
+    }
+}
+
+pub struct Connection {
+    pub distance: f32,
+    pub j1: u32,
+    pub j2: u32,
+}
+
+impl Connection {
+    fn new(distance: f32, j1: u32, j2: u32) -> Connection {
+        Connection { distance, j1, j2 }
+    }
 }
 
 #[derive(Component)]
 pub struct Track {
-    joints: Vec<Joint>,
-    connections: Vec<Connection>,
+    pub joints: Vec<Joint>,
+    pub connections: Vec<Connection>,
+}
+
+impl Track {
+    pub fn simple(distance: f32) -> Track {
+        let joints = vec![Joint::empty(0), Joint::empty(1)];
+        let connections = vec![
+            Connection::new(distance, 0, 1),
+            Connection::new(distance, 1, 0),
+        ];
+
+        Track {
+            joints,
+            connections,
+        }
+    }
+
+    pub fn attach_at(&mut self, id: u32, entiy: Entity) {
+        let f = self.joints.iter_mut().find(|x| x.id == id);
+
+        match f {
+            Some(joint) => {
+                joint.connected_entity = Some(entiy);
+                joint.connected = true;
+            }
+            None => println!("Joint {} not found", id),
+        }
+    }
+
+    pub fn find_joint(&self, id: u32) -> Option<&Joint> {
+        return self.joints.iter().find(|x| x.id == id);
+    }
+
+    pub fn find_joint_by_entity(&self, entiy: Entity) -> Option<&Joint> {
+        return self
+            .joints
+            .iter()
+            .find(|&x| x.connected_entity.is_some() && x.connected_entity.unwrap() == entiy);
+    }
+
+    pub fn find_connections_for_joint(&self, id: u32) -> Vec<&Connection> {
+        return self
+            .connections
+            .iter()
+            .filter(|&x| x.j1 == id || x.j2 == id)
+            .collect();
+    }
+
+    pub fn log_joints(&self) {
+        for joint in self.joints.iter() {
+            println!("joint id {}, connected {}", joint.id, joint.connected);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod components;
 
 use bevy::prelude::*;
+use components::{locomotive::Locomotive, track::Track};
 
 fn main() {
     App::new()
@@ -14,8 +15,8 @@ pub struct NodePlugin;
 impl Plugin for NodePlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(boot)
-            .add_startup_system(add_nodes)
-            .add_system(log_nodes);
+            .add_startup_system(add_tracks)
+            .add_system(advance_logomotive);
     }
 }
 
@@ -26,18 +27,94 @@ fn boot() {
 #[derive(Component)]
 struct Node;
 
-fn add_nodes(mut commands: Commands) {
-    let node_1 = commands.spawn(Node).id();
-    let node_2 = commands.spawn(Node).id();
-    let node_3 = commands.spawn(Node).id();
+fn add_tracks(mut commands: Commands) {
+    let mut track1 = Track::simple(10.0);
+    let mut track2 = Track::simple(10.0);
+    let mut track3 = Track::simple(10.0);
 
-    commands.entity(node_1).add_child(node_2);
-    commands.entity(node_2).add_child(node_3);
-    commands.entity(node_3).add_child(node_1);
+    let tr1 = commands.spawn_empty().id();
+    let tr2 = commands.spawn_empty().id();
+    let tr3 = commands.spawn_empty().id();
+
+    track1.attach_at(0, tr3);
+    track1.attach_at(1, tr2);
+
+    track2.attach_at(0, tr1);
+    track2.attach_at(1, tr3);
+
+    track3.attach_at(0, tr2);
+    track3.attach_at(1, tr1);
+
+    commands.entity(tr1).insert(track1);
+    commands.entity(tr2).insert(track2);
+    commands.entity(tr3).insert(track3);
+
+    commands.spawn(Locomotive::new(2.0, tr1, 1));
 }
 
-fn log_nodes(query: Query<(Entity, &Children), With<Node>>) {
-    for (_, children) in &query {
-        println!("children {}", children.len());
+fn log_tracks(query: Query<&Track>) {
+    for track in &query {
+        track.log_joints();
+    }
+}
+
+fn advance_logomotive(
+    time: Res<Time>,
+    mut loco_query: Query<&mut Locomotive>,
+    track_query: Query<&Track>,
+) {
+    for mut loco in loco_query.iter_mut() {
+        let dist_delta = loco.speed * time.delta_seconds();
+        loco.distance -= dist_delta;
+
+        if loco.distance < 0.0 {
+            if let Ok(current) = track_query.get(loco.location) {
+                let next_track_entity = current
+                    .find_joint(loco.target)
+                    .and_then(|joint| joint.connected_entity);
+                let next_track = next_track_entity.and_then(|next| track_query.get(next).ok());
+
+                assert!(next_track.is_some(), "next_track not found");
+
+                let connection_joint =
+                    next_track.and_then(|track| track.find_joint_by_entity(loco.location));
+
+                assert!(connection_joint.is_some(), "connection_joint not found");
+
+                if next_track.is_some() && connection_joint.is_some() {
+                    let connections = next_track
+                        .unwrap()
+                        .find_connections_for_joint(connection_joint.unwrap().id);
+
+                    let next_connection = connections
+                        .iter()
+                        .find(|&&x| x.j1 == connection_joint.unwrap().id);
+
+                    assert!(next_connection.is_some(), "next_connection not found");
+
+                    let target_joint = next_connection.map(|x| x.j2);
+
+                    assert!(target_joint.is_some(), "target_joint not found");
+
+                    if target_joint.is_some() {
+                        println!(
+                            "assigning location: {}, target: {}",
+                            next_track_entity.unwrap().index(),
+                            target_joint.unwrap(),
+                        );
+
+                        loco.target = target_joint.unwrap();
+                        loco.location = next_track_entity.unwrap();
+                        loco.distance = next_connection.unwrap().distance + loco.distance;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn log_locomotive(query: Query<&Locomotive>) {
+    for loco in &query {
+        loco.log_location();
     }
 }
